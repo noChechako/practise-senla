6e0d5f469382fb3712575c1a9dc4c80a5fc1f44b
Jenya Habinski <tabey123@mail.ru>
2021-01-26 14:48:37 +0300

    create

--- src/Films/Content.js
+++ src/Films/Content.js
@@ -6,45 +6,82 @@ import React, { Component } from 'react';
 class Content extends Component {
     constructor(props) {
         super(props)
-        this.state={};
-        this.getFilms();
-        
-    }
-    getFilms() {
-        let movieRows = [];
+        this.state = {
+            error: null,
+            isLoaded: false,
+            rows: []
+        };
 
+
+
+    }
+    // getFilms() {
+    //     let movieRows = [];
+
+    //     const urlString = 'https://api.themoviedb.org/3/search/movie?query=marvel&api_key=6be28322108b286b7e45d15ac68bb3b2';
+    //     fetch(urlString)
+    //         .then(function (response) {
+    //             return response.json()
+    //         })
+    //         .then(
+    //             (result) => {
+    //                 this.setState({
+    //                     rows: result
+    //                 });
+    //                 console.log(result)
+    //             })
+
+
+    // }
+    componentDidMount() {
         const urlString = 'https://api.themoviedb.org/3/search/movie?query=marvel&api_key=6be28322108b286b7e45d15ac68bb3b2';
         fetch(urlString)
             .then(function (response) {
+                console.log(response)
                 return response.json()
             })
-            .then(function (data) {
-                let info = data.results;
-                console.log(info)
-                info.forEach(movie => {
-                    const movieRow = <Element movie={movie} />
-                    movieRows.push(movieRow);
-                    
-                });
-                console.log(this)
-            this.setState({rows:movieRows})
-            // this.state.rows=movieRows;
-            // this.setState({}); 
+            .then((result) => {
 
-            })
-        
+                this.setState({
+                    isLoaded: true,
+                    rows: result
+                });
+            },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
 
     }
-
-
-
 
     render() {
-        return (<div class="content" >
-            {this.state.rows}
-        </div>)
 
+        const { error, isLoaded, rows } = this.state;
+// console.log(rows)
+        if (error) {
+            return (
+                <span>
+                    Error: {error.message}
+                </span>
+            );
+        } else if (!isLoaded) {
+            return (
+                <span>
+                    Loading...
+                </span>
+            );
+        } else {
+            return (
+                <div className="content">
+                    {rows.results.map(row => <div class="content__elem">{row.original_title}</div>)}
+                </div>
+            );
+        }
     }
+
 }
 
 export default Content;
